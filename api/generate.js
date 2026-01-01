898672d34240878d26fe6e58d747f8062400d4b2
sarimaxllc-collab <sarimaxllc@gmail.com>
2026-01-02 03:17:58 +0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch(https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}, {
+    const response = await fetch(https://generativelanguage.googleapis.com/v1beta/models/GEMINI_3.0_FLASH:generateContent?key=${apiKey}, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
